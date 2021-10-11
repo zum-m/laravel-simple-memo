e05e1e5367e22df4d325e1b6c82e70e4315556e0
zum-m <21039m@gmail.com>
2021-10-11 17:42:37 +0900

    レイアウトbootstrapのcssを開発

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,9 +78,27 @@
         <!-- 3カラムに変更 -->
         <main class="py-4">
             <div class="row">
-                <div class="col-md-2">左カラム</div>
-                <div class="col-md-4">中央カラム</div>
-                <div class="col-md-6">
+                <div class="col-md-2 p-0">
+                    <div class="card">
+                        <div class="card-header">左カラム</div>
+                        <div class="card-body">
+                            <!-- <h5 class="card-title">Special title treatment</h5> -->
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 p-0">
+                    <div class="card">
+                        <div class="card-header">中央カラム</div>
+                        <div class="card-body">
+                            <!-- <h5 class="card-title">Special title treatment</h5> -->
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 p-0">
                     右カラム
                     @yield('content')
                 </div>
