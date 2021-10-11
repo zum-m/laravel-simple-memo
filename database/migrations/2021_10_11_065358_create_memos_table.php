ca98509eb5d2a80fc73d83c0c623181317ce3a58
zum-m <21039m@gmail.com>
2021-10-11 16:07:01 +0900

    ４つのマイグレーションファイルのテーブル定義（テーブル構造）を実装

--- database/migrations/2021_10_11_065358_create_memos_table.php
+++ database/migrations/2021_10_11_065358_create_memos_table.php
@@ -14,8 +14,16 @@ class CreateMemosTable extends Migration
     public function up()
     {
         Schema::create('memos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('id', true);
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+            // 論理削除を定義→deleted_atを自動生成
+            $table->softDeletes();
+            // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接書いてます
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            // 🟡外部キー制約とは？unsignedのとこ２３
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
