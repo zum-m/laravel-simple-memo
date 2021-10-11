d36dd8a1e595e564dac9aa0053c145d27bedbeec
zum-m <21039m@gmail.com>
2021-10-11 22:45:04 +0900

    削除のルートの追加/削除ボタンの追加

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,6 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::post('/store', [HomeController::class, 'store'])->name('store');
 Route::get('/edit/{id}', [HomeController::class, 'edit'])->name('edit');
 Route::post('/update', [HomeController::class, 'update'])->name('update');
+Route::post('/destroy', [HomeController::class, 'destroy'])->name('destroy');
+
 
