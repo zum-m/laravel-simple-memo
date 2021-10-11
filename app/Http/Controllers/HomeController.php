733097913de72d88072aa6b31ded4be3c582188c
zum-m <21039m@gmail.com>
2021-10-11 22:52:55 +0900

    削除のコントローラーメソッドの追加。論理削除

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -71,5 +71,15 @@ class HomeController extends Controller
         return redirect( route('home'));
     }
 
+    public function destroy(Request $request)
+    {
+        $posts = $request->all();
+
+        // Memo::where('id', $posts['memo_id'])->delete();←NGこれやると物理削除
+        Memo::where('id', $posts['memo_id'])->update(['deleted_at' => date("Y-m-d H:i:s", time())]);
+
+        return redirect( route('home') );
+    }
+
 
 }
