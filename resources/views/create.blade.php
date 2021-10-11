40d907d2a7a7fd6da49443490f127ec29882d788
zum-m <21039m@gmail.com>
2021-10-11 18:01:57 +0900

    formタグには@csrfを追加しなければならない。セキュリティ

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,6 +4,7 @@
 <div class="card">
     <div class="card-header">新規メモ作成</div>
     <form class="card-body" action="/store" method="POST">
+        @csrf
         <div class="form-group">
             <!-- <label for="exampleFormControlTextarea1">Example textarea</label> -->
             <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力"></textarea>
