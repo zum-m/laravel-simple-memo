b7bc8e30405d349183227d2d56b3288387af9523
zum-m <21039m@gmail.com>
2021-10-12 18:15:12 +0900

    エラー発生

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -11,26 +11,20 @@ class Memo extends Model
 
     public function getMyMemo(){
         $query_tag = \Request::query('tag');
+
+        $memos = Memo::select('memos.*')
+        ->where('user_id', '=', \Auth::id())
+        ->whereNull('deleted_at')
+        ->orderBy('updated_at', 'DESC');// ASC＝小さい順、DESC=大きい順
+
+
         // 46 分岐処理
         if ( !empty($query_tag)){
-
-            $memos = Memo::select('memos.*')
-             ->leftJoin('memo_tags', 'memo_tags.memo_id' ,'=', 'memos.id' )
-             ->where('memo_tags.tag_id', '=', $query_tag)
-                ->where('user_id', '=', \Auth::id())
-                ->whereNull('deleted_at')
-                ->orderBy('updated_at', 'DESC')// ASC＝小さい順、DESC=大きい順
-                ->get();
-
-        }else{
-
-            $memos = Memo::select('memos.*')
-                ->where('user_id', '=', \Auth::id())
-                ->whereNull('deleted_at')
-                ->orderBy('updated_at', 'DESC')// ASC＝小さい順、DESC=大きい順
-                ->get();
-
+            $query->leftJoin('memo_tags', 'memo_tags.memo_id' ,'=', 'memos.id' )
+                ->where('memo_tags.tag_id', '=', $query_tag);
         }
+
+        $memos = $query->get();
     return $memos;
 
     }
