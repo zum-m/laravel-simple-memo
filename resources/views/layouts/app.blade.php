bb2ba0557a2e545124fe64bf27f272ffba41aec5
zum-m <21039m@gmail.com>
2021-10-12 19:05:21 +0900

    50 アイコンの追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,7 +96,10 @@
                 </div>
                 <div class="col-md-4 p-0">
                     <div class="card">
-                        <div class="card-header">メモ一覧</div>
+                        <div class="card-header">
+                            メモ一覧
+                            <a href="{{route('home')}}"><i class="fas fa-plus"></i></a>
+                        </div>
                         <div class="card-body">
                     @foreach($memos as $memo)
                           <a href="/edit/{{$memo['id']}}" class="card-text d-block">{{ $memo['content'] }}</a>
