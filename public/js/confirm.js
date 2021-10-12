a2364850a7c60d11ccf79aed3caa28cdfdc640c0
zum-m <21039m@gmail.com>
2021-10-12 18:50:17 +0900

    49JavaScriptの埋め込み方/@yield/@section/で連携

---
+++ public/js/confirm.js
@@ -0,0 +1,11 @@
+function deleteHandle(event){
+    // 一旦フォームをストップ
+    event.preventDefault();
+    if(window.confirm('本当に削除していいですか？')){
+      // 削除OKならformを再開
+      document.getElementById('delete-form').submit();
+      }else{
+          alert('キャンセルしました');
+      }
+  }
+  
