18beb470f56457effc0e5c1cd8bb2ba08cc129c8
zum-m <89333894+zum-m@users.noreply.github.com>
2022-01-23 22:34:42 +0900

    消去機能を実装。ボタンを押したらruote('index')のリダイレクトで読み出される、 DBからのselectデータをもとにcontentの内容が表示されないようにしたい。 ⭐️🟡index()の中にあるselect文ないみを確認すべき。

--- laravelmemo2/resources/views/edit.blade.php
+++ laravelmemo2/resources/views/edit.blade.php
@@ -2,7 +2,17 @@
 
 @section('content')
 <div class="card">
-    <div class="card-header">メモ編集</div>
+    <div class="card-header">
+        メモ編集
+        <form action="{{ route('destroy') }}" method="POST">
+            @csrf
+            <!-- 🟡valueは何のために必要？？ 🟡その部分動画とかえて抜いている-->
+            <!-- 🟦↪︎valerを除いてしまうと、home.controllerにpostでわたるnemeの中身が渡らないことがわかった -->
+            <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}">
+
+            <button>削除</button>
+        </form>
+    </div>
     <form class="card-body" action="{{ route('update') }}" method="POST">
     <!-- または<form class="card-body" action="/store" method="POST"> -->
         @csrf
