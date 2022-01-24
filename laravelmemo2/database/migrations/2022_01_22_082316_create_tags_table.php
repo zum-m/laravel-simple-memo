2c8ef983d7a1b477bc930e8a1285bab1af51aea7
zum-m <89333894+zum-m@users.noreply.github.com>
2022-01-24 19:26:05 +0900

    （home画面）create画面でのタグ表示の実装
    新しくtagsテーブルをviewに返すindexメソッドの処理を追加
    
    create()でのトランザクションの実装
    ↪︎今までのhttp通信の部分からさらに細分化した処理。
    ユーザーの入力内容を受け取ってインサート（データの登録）
    
    view画面でのループによるinputとlavelの追加

--- laravelmemo2/database/migrations/2022_01_22_082316_create_tags_table.php
+++ laravelmemo2/database/migrations/2022_01_22_082316_create_tags_table.php
@@ -21,7 +21,7 @@ class CreateTagsTable extends Migration
             // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接書いてます
             $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->foreign('user_id')->references('id')->on('users'); 
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
